23bb0ada7c199d6c69a1c33fa753f7efba3ea95f
Lucas Dino Nolte <hello@lucas-nolte.com>
2020-08-26 20:13:08 +0200

    Setup 11ty for docs

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+module.exports = () => {
+  return {
+    dir: {
+      input: 'docs',
+      data: 'data',
+      includes: 'includes',
+      layouts: 'layouts',
+      output: 'dist',
+    },
+  }
+}
