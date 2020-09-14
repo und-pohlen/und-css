308e2fc60efeb5976971e62ea82ef6116b196967
Lucas Dino Nolte <hello@lucas-nolte.com>
2020-09-14 18:52:51 +0200

    Add basic auto generated utility docu

---
+++ docs/data/utilities.js
@@ -0,0 +1,36 @@
+const sass = require('node-sass')
+const path = require('path')
+const fs = require('fs')
+
+module.exports = async function() {
+  const filePath = path.resolve(__dirname, '../../src/styles.scss')
+  const scssStyles = fs.readFileSync(filePath, 'utf8')
+  const { css } = sass.renderSync({
+    data: scssStyles,
+    includePaths: [path.resolve(__dirname, '../../src')],
+  })
+
+  const pattern = /((\/\*(\=+)([\s\S]+?)\*\/))/mg
+  const hashPattern = /(@[a-z]+)(\:\s)(.+)/mg
+
+  return css.toString().match(pattern).map((item) => {
+    let resp = {}
+    item.match(hashPattern).forEach((match) => {
+      const line = match.split(hashPattern)
+      resp[line[1].replace('@', '')] = line[3]
+    })
+
+    return {
+      name: resp.name || 'Not Documented',
+      classname: `${resp.classname}####`,
+      properties: resp.properties.split(' '),
+      responsive: !!resp.properties.responsive,
+      values: resp.values.replace('(', '').replace(')', '').replace(/\"/mg, '').replace(/\s/mg, '').split(',').map((item) => {
+        const i = item.split(':')
+        let prop = {}
+        prop[`${resp.classname}${i[0].replace(/\\+/mg, '')}`] = i[1]
+        return prop
+      }),
+    }
+  })
+}
